b11a3ad758133d1ebd48c6f24217cc3026cad2f6
rdawson24 <ryan.dawson@student.uts.edu.au>
2021-10-03 01:35:43 +1000

    Added automatic clockwise pacman movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,43 +18,43 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<Animator>();
 
         //User control functionality
-        // if(!Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)){
-        //     animator.SetBool("Active", false);
-        //     animator.SetBool("Up", false);
-        //     animator.SetBool("Down", false);
-        //     animator.SetBool("Left", false);
-        //     animator.SetBool("Right", false);
-        // }
-        // if(Input.GetKey(KeyCode.UpArrow)){
-        //     animator.SetBool("Active", true);
-        //     animator.SetBool("Up", true);
-        //     animator.SetBool("Down", false);
-        //     animator.SetBool("Left", false);
-        //     animator.SetBool("Right", false);
-        // }
-        // if(Input.GetKey(KeyCode.DownArrow)){
-        //     animator.SetBool("Active", true);
-        //     animator.SetBool("Up", false);
-        //     animator.SetBool("Down", true);
-        //     animator.SetBool("Left", false);
-        //     animator.SetBool("Right", false);
-        // }
-        // if(Input.GetKey(KeyCode.LeftArrow)){
-        //     animator.SetBool("Active", true);
-        //     animator.SetBool("Up", false);
-        //     animator.SetBool("Down", false);
-        //     animator.SetBool("Left", true);
-        //     animator.SetBool("Right", false);
-        // }
-        // if(Input.GetKey(KeyCode.RightArrow)){
-        //     animator.SetBool("Active", true);
-        //     animator.SetBool("Up", false);
-        //     animator.SetBool("Down", false);
-        //     animator.SetBool("Left", false);
-        //     animator.SetBool("Right", true);
-        // }
-        // if(Input.GetKey(KeyCode.Space)){
-        //     animator.SetTrigger("Dead");
-        // }
+        if(!Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)){
+            animator.SetBool("Active", false);
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", false);
+        }
+        if(Input.GetKey(KeyCode.UpArrow)){
+            animator.SetBool("Active", true);
+            animator.SetBool("Up", true);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", false);
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            animator.SetBool("Active", true);
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", true);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", false);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            animator.SetBool("Active", true);
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", true);
+            animator.SetBool("Right", false);
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            animator.SetBool("Active", true);
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", true);
+        }
+        if(Input.GetKey(KeyCode.Space)){
+            animator.SetTrigger("Dead");
+        }
     }
 }
